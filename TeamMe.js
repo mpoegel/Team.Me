4f9a55bb33dea30a2a6a04d0414c1e4d10c8aa00
Matt Poegel <mpoegel@comcast.net>
2014-11-01 11:59:05 -0400

    jumped the gun on router

--- TeamMe.js
+++ TeamMe.js
@@ -4,4 +4,10 @@
 // MongoDB
 Events = new Meteor.Collection("Events");
 
+Router.route('/', function () {
+// render the Home template with a custom data context
+this.render('home', {data: {title: 'TeamMe'}});
+});
+
+
 Router.route('/form');
